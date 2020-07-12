6a2e54c93055e88f59767751375f92d68ccc7da7
emergent <yosikawa@gmail.com>
2020-07-12 15:59:27 +0900

    modify Kotlin code and build.gradle for calling NDK function

--- app/src/main/java/org/nitamago/rustndksample/MainActivity.kt
+++ app/src/main/java/org/nitamago/rustndksample/MainActivity.kt
@@ -1,11 +1,25 @@
 package org.nitamago.rustndksample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        init {
+            System.loadLibrary("rustndk")
+        }
+    }
+
+    external fun hello(input: String?): String?
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        kotlin.runCatching {
+            val tv = findViewById<TextView>(R.id.text_view)
+            tv.text = hello("Rust")
+        }
     }
 }
